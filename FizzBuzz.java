9c5e5f1e4fad92faa9d554db89bb698bea7392a3
Todymaster <81971172+Todymaster@users.noreply.github.com>
2025-01-15 09:20:33 -0500

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,63 @@
+public class MyClass {
+  public static void main(String args[]) {
+    int x = 3;
+    int y = 5;
+    int z = 15;
+    int a = 69;
+    
+    if (x % 15 == 0){
+        System.out.println("Fizzbuzz");
+    }
+    else if (x % 3 == 0){
+        System.out.println("Fizz");
+    }
+    else if (x % 5 == 0){
+        System.out.println("Buzz");
+    }
+    else {
+        System.out.println("The number is not fizzing or buzzing");
+    }
+    
+    
+    if (y % 15 == 0){
+        System.out.println("Fizzbuzz");
+    }
+    else if (y % 3 == 0){
+        System.out.println("Fizz");
+    }
+    else if (y % 5 == 0){
+        System.out.println("Buzz");
+    }
+    else {
+        System.out.println("The number is not fizzing or buzzing");
+    }
+    
+    
+    if (z % 15 == 0){
+        System.out.println("Fizzbuzz");
+    }
+    else if (z % 3 == 0){
+        System.out.println("Fizz");
+    }
+    else if (z % 5 == 0){
+        System.out.println("Buzz");
+    }
+    else {
+        System.out.println("The number is not fizzing or buzzing");
+    }
+    
+    
+    if (a % 15 == 0){
+        System.out.println("Fizzbuzz");
+    }
+    else if (a % 3 == 0){
+        System.out.println("Fizz");
+    }
+    else if (a % 5 == 0){
+        System.out.println("Buzz");
+    }
+    else {
+        System.out.println("The number is not fizzing or buzzing");
+    }
+  }
+}
