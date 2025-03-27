0d781fa18a3303ccf3ff7e32f7afb669c14b10c9
Todymaster <81971172+Todymaster@users.noreply.github.com>
2025-03-27 09:08:44 -0400

    Add files via upload

---
+++ Coffee/CoffeeShop.java
@@ -0,0 +1,20 @@
+public class CoffeeShop {
+    // TODO 1: Change the class name to CoffeeShop
+
+    public static void main(String[] args) {
+
+        // TODO 2: Create three CoffeeOrder objects
+        // Use a different constructor each time
+        CoffeeOrder order1 = new CoffeeOrder();
+        CoffeeOrder order2 = new CoffeeOrder("gigantic");
+        CoffeeOrder order3 = new CoffeeOrder("humungous", "white");
+
+        // TODO 3: Call describeOrder() on each and print the result
+        System.out.println(order1.describeOrder());
+        System.out.println(order2.describeOrder());
+        System.out.println(order3.describeOrder());
+
+        // TODO 4: Print the total number of orders using
+        System.out.println("Total number of orders: " + CoffeeOrder.getTotalOrders());
+    }
+}
